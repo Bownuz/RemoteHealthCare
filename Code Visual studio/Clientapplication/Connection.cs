bdbb07d7ed35a68b1494000bca372aa1ba2002e6
Bownuz <fedde.snelder@gmail.com>
2024-09-16 14:05:27 +0200

    Revert "Klasses toegevoegd vanuit het klassediagram"
    This reverts commit dafe764a5fda450f7c80e73b126a8b8cba76bde7.

---
+++ Code Visual studio/Clientapplication/Connection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientapplication {
+    internal class Connection {
+        HeartRateConnection heartRateConnection = new HeartRateConnection();
+        BicycleConnection bicycleConnection = new BicycleConnection();
+        ServerConnection serverConnection = new ServerConnection();
+    }
+}
