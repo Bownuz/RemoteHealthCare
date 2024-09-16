f092d3c00cade930980e84dd7898b43e592b53e7
Bownuz <fedde.snelder@gmail.com>
2024-09-16 13:45:08 +0200

    Klasses toegevoegd vanuit het klassediagram

---
+++ Code Visual studio/Clientapplication/ServerConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clientapplication {
+    internal class ServerConnection {
+        Socket connection;
+        ServerReciever serverReciever = new ServerReciever();
+    }
+}
