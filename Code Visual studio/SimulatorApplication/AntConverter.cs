34400d9e76b1b68730c7ea4b1e2dcb35052f9edf
Bownuz <fedde.snelder@gmail.com>
2024-09-19 16:23:28 +0200

    Tijdelijke tussenstand simulator

--- Code Visual studio/SimulatorApplication/AntConverter.cs
+++ Code Visual studio/SimulatorApplication/AntConverter.cs
@@ -6,5 +6,23 @@ using System.Threading.Tasks;
 
 namespace SimulatorApplication {
     internal class AntConverter {
+        public static void Main(string[] args) {
+            Console.WriteLine(Convert());
+        }
+
+        public static String Convert() {
+            Random random = new Random();
+            int range = 70;
+            double rDouble = random.NextDouble() * range;
+            
+            rDouble = rDouble / 3.6;
+            rDouble = rDouble / 0.01;
+            byte[] bytes = BitConverter.GetBytes(rDouble);
+            //int speedLSB = bytes[0];
+            //int speedMSB = bytes[1];
+            Console.WriteLine(rDouble);
+            Console.WriteLine(BitConverter.ToString(bytes));
+            return $"A4 09 4E 05 10 F8 00 C4 {bytes[0]:X} {bytes[1]:X} 60 20 8A";
+        }
     }
 }
