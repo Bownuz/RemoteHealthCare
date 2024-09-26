70e55533092db15bec809351e3300f8080720ac9
siemh <siemhorst0@gmail.com>
2024-09-26 15:14:37 +0200

    server aangemaakt

---
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/ConnectionService.cs
@@ -0,0 +1,46 @@
+﻿using DataProtocol;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConnectionService
+{
+    class ConnectionService
+    {
+        static void Main(string[] args)
+        {
+            List<Thread> threads = new List<Thread>();
+       
+            TcpListener listener = new TcpListener(IPAddress.Any, 4789);
+            listener.Start();
+
+            while (true) {
+                Console.WriteLine("Waiting for connections.......");
+
+                //accepting client and making a new thread for this client
+                TcpClient client = listener.AcceptTcpClient();
+                
+                Thread thread = new Thread(HandleClientThread);
+                threads.Add(thread);
+                thread.Start(client);
+            }
+            
+
+            void HandleClientThread(object obj) {
+                TcpClient client = obj as TcpClient;
+
+            while (true) {
+                string recived = DataProtocol.Messages.ReciveMessage(client);
+                Console.WriteLine("Recived: {0}", recived);
+                DataProtocol.Messages.SendMessage(client, "Message: " + recived + " Recived");
+            }
+                client.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+    }
+}
---
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/DataProtocol.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataProtocol {
+    class Messages {
+        public static string ReciveMessage(TcpClient client) {
+            var stream = new StreamReader(client.GetStream(), Encoding.ASCII);
+            {
+                return stream.ReadLine();
+            }
+        }
+
+        public static void SendMessage(TcpClient client, string message) {
+            var stream = new StreamWriter(client.GetStream(), Encoding.ASCII, -1, true);
+            {
+                stream.WriteLine(message);
+                stream.Flush();
+            }
+        }
+
+    }
+}
