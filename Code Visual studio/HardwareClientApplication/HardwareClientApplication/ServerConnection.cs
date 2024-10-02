a2388c4f45a1e358dd42f1f14b77f77bb3c44ebe
Bownuz <fedde.snelder@gmail.com>
2024-10-02 11:29:31 +0200

    Json werkt niet

--- Code Visual studio/HardwareClientApplication/HardwareClientApplication/ServerConnection.cs
+++ Code Visual studio/HardwareClientApplication/HardwareClientApplication/ServerConnection.cs
@@ -11,22 +11,47 @@ using System.Text.Json;
 
 namespace HardwareClientApplication {
     internal class ServerConnection {
+        static TcpClient client;
         public static void HandleConnection() {
-            TcpClient client = new TcpClient("145.49.11.240", 4789);
-            while(true) {
-                WriteTextMessage(client, "Test");
-                WriteTextMessage(client, "Bye");
+            client = new TcpClient("145.49.11.240", 4789);
+            while (true) {
+                //WriteTextMessage(client, "Test");
+                //WriteTextMessage(client, "Bye");
 
-                if (ReadTextMessage(client) == "Bye")
-                    break;
+                if (ReadTextMessage(client) == "Bye") { 
+                break;
+                //} else if (ReadTextMessage(client)) {
+
+                }
             }
             client.Close();
         }
 
+<<<<<<< Updated upstream
         public static void WriteTextMessage(TcpClient client, string message) {
             Dictionary<String, String> test = new Dictionary<String, String fietsSnelheid> =
             string sendJsonMessage = JsonSerializer.Serialize(csn);
             Console.WriteLine(sendJsonMessage);
+=======
+        //public static void WriteTextMessage(TcpClient client, string message) {
+        //    Dictionary<String, String> data = new Dictionary<String, String>;
+        //    String sendJsonMessage = JsonSerializer.Serialize(data);
+        //    Console.WriteLine(sendJsonMessage);
+        //    var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
+        //    {
+        //        stream.WriteLine(sendJsonMessage);
+        //        stream.Flush();
+        //    }
+        //}
+
+        public struct Data {
+
+        }
+
+        public static void WriteData(TcpClient client, Dictionary<String, String> data) {
+            String sendJsonMessage = JsonSerializer.Serialize(data);
+            Console.Write(sendJsonMessage);
+>>>>>>> Stashed changes
             var stream = new StreamWriter(client.GetStream(), Encoding.ASCII);
             {
                 stream.WriteLine(sendJsonMessage);
