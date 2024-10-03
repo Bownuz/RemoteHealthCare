21ac6d773c1c753737630a8794f49c87e8f7d437
Mohammed <m.lahlal@student.avans.nl>
2024-10-03 13:32:54 +0200

    Kleine aanpassing

--- Code Visual studio/HardwareClientApplication/HardwareClientApplication/HeartRateMonitor.cs
+++ Code Visual studio/HardwareClientApplication/HardwareClientApplication/HeartRateMonitor.cs
@@ -13,36 +13,19 @@ namespace ConnectionImplemented {
                 return 0;
             }
         }
-
-<<<<<<< Updated upstream
-        private int ExtractHeartRate(byte[] data) {
-            // Controleer of de data minstens 2 bytes bevat
-            if (data.Length < 2) {
-=======
         private int ExtractHeartRate(byte[] data)
-        {
-         
+        {        
             if (data.Length < 2)
             {
->>>>>>> Stashed changes
                 return -1;
             }
          
-            int heartRate = data[1];
-
-<<<<<<< Updated upstream
-            // Controleer of de hartslag binnen een realistische range valt
-            if (heartRate >= 30 && heartRate <= 220) {
-=======
-            
+            int heartRate = data[1];                  
             if (heartRate >= 30 && heartRate <= 220)
             {
->>>>>>> Stashed changes
                 return heartRate;
-            }
-
-           
-            return -1;
+            }         
+           return -1;
         }
     }
 }
