55f7ae504f2d4961a67172fbb74d29de9394d570
davidVeer <davidvdveer05@gmail.com>
2024-10-11 14:42:44 +0200

    adding seperation between doctor and client threads

--- Code Visual studio/ServerTest2.1/ServerTest2.1/ConnectionService.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/ConnectionService.cs
@@ -14,19 +14,26 @@ namespace ConnectionService
             //server
             List<Thread> threads = new List<Thread>();
 
-            TcpListener listener = new TcpListener(IPAddress.Any, 4789);
-            listener.Start();
+            TcpListener Clientlistener = new TcpListener(IPAddress.Any, 4789);
+            TcpListener DoctorListener = new TcpListener(IPAddress.Any, 4790);
+            Clientlistener.Start();
+            DoctorListener.Start();
 
             while (true)
             {
                 Console.WriteLine("Starting up server and waiting for connections.....");
 
                 //accepting client and making a new thread for this client
-                TcpClient client = listener.AcceptTcpClient();
+                TcpClient client = Clientlistener.AcceptTcpClient();
+                Thread clientThread = new Thread(() => HandleClientThread(client));
+                threads.Add(clientThread);
+                clientThread.Start();
 
-                Thread thread = new Thread(() => HandleClientThread(client));
-                threads.Add(thread);
-                thread.Start();
+                //accepting docotr and making a new thread for this doctor
+                TcpClient doctor = DoctorListener.AcceptTcpClient();
+                Thread doctorThread = new Thread(() => HandleDoctorThread(doctor));
+                threads.Add(doctorThread);
+                doctorThread.Start();
             }
 
 
@@ -36,17 +43,25 @@ namespace ConnectionService
                 Session currentSession = new Session();
                 while (true)
                 {
-                    string recieved;
-                    if ((recieved = DataProtocol.Messages.ReciveMessage(client)) != null)
-                    { 
-                        Console.WriteLine("Recived: {0}", recieved);
-                        ClientRecieveData jsonData = JsonSerializer.Deserialize<ClientRecieveData>(recieved);
-                        Console.WriteLine("Speed: " + jsonData.BicycleSpeed + "\nHeartrate: " + jsonData.Heartrate + "\nDate: " + jsonData.dateTime);
-                        
+                    if (client.Connected) {
+                        string recieved;
+                        if ((recieved = DataProtocol.Messages.ReciveMessage(client)) != null)
+                        {
+                            Console.WriteLine("Recived: {0}", recieved);
+                            ClientRecieveData jsonData = JsonSerializer.Deserialize<ClientRecieveData>(recieved);
+                            Console.WriteLine("Speed: " + jsonData.BicycleSpeed + "\nHeartrate: " + jsonData.Heartrate + "\nDate: " + jsonData.dateTime);
+                            currentSession.addMessagesRecived(recieved);
+                        }
                     }
-
+                    break;
                 }
             }
+
+            void HandleDoctorThread(TcpClient doctor) { 
+            
+            
+            }
+
         }
         
     }
