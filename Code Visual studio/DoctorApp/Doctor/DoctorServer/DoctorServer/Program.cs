0c499eec592be68cd58fd20cb61ba8be910ee08f
Mohammed <m.lahlal@student.avans.nl>
2024-10-15 19:05:36 +0200

    Kleine servertje gemaakt om te testen

---
+++ Code Visual studio/DoctorApp/Doctor/DoctorServer/DoctorServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets; 
+using DoctorApp; 
+
+class Program {
+    static void Main(string[] args) {
+        TcpClient doctorClient = new TcpClient("127.0.0.1", 4790); 
+        DoctorLogin login = new DoctorLogin();
+
+        if(login.Login(doctorClient)) {
+            Console.WriteLine("Inloggen succesvol! Beginnen met monitoren...");
+
+            DoctorMonitor monitor = new DoctorMonitor();
+            monitor.StartMonitoring(doctorClient);
+        }
+        else {
+            Console.WriteLine("Inloggen mislukt!");
+        }
+    }
+}
