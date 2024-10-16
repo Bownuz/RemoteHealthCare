cd1032cfae8c01adb3713009ff56f85f8429018b
Mohammed <m.lahlal@student.avans.nl>
2024-10-16 11:23:02 +0200

    changed the GUI

--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/MainWindow.xaml.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/MainWindow.xaml.cs
@@ -1,25 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.Net.Sockets;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace DoctorApplication {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window {
+        private TcpClient doctorClient;
+
         public MainWindow() {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e) {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Password;
+
+            if(username == "doctor" && password == "password123") 
+            {
+                // Verbind met de server
+                ConnectToServer();
+            }
+            else {
+                MessageBox.Show("Foutieve gebruikersnaam of wachtwoord.");
+            }
+        }
+
+        private void ConnectToServer() {
+            try {
+                doctorClient = new TcpClient("127.0.0.1", 4790); 
+                MessageBox.Show("Verbinding gemaakt met de server!");
+                
+                StartMonitoring();
+            }
+            catch(Exception ex) {
+                MessageBox.Show("Kan geen verbinding maken met de server: " + ex.Message);
+            }
+        }
+
+        private void StartMonitoring() {
+            
         }
     }
 }
