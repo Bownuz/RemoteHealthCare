a3e738461bc151a4fc0294ddacfa5b26ad9c7787
Mohammed <m.lahlal@student.avans.nl>
2024-10-16 13:49:10 +0200

    Changed the ip-address

--- Code Visual studio/HardwareClientApplication/HardwareClientApplication/ClientMain.cs
+++ Code Visual studio/HardwareClientApplication/HardwareClientApplication/ClientMain.cs
@@ -1,3 +1,4 @@
+using Avans.TI.BLE;
 using HardwareClientApplication;
 using System;
 using System.Net;
@@ -7,19 +8,19 @@ using System.Threading.Tasks;
 
 namespace ConnectionImplemented {
     internal class ClientMain {
-
         static async Task Main(string[] args) {
             ListDisplay.ShowDeviceList();
 
             HeartRateMonitor heartRateMonitor = new HeartRateMonitor();
             Ergometer ergometer = new Ergometer();
-            BleDevice[] bleDevices = { heartRateMonitor};
+            BleDevice[] bleDevices = { heartRateMonitor };
 
             DataHandler handler = new DataHandler(bleDevices);
 
-            TcpClient client = new TcpClient("192.168.178.101", 4789);
+            TcpClient client = new TcpClient("192.168.178.58", 4789); 
             Thread connectionThread = new Thread(() => ServerConnection.HandleConnection(client, handler));
             connectionThread.Start();
         }
     }
 }
+
