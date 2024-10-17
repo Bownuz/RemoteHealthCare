51af1eb444bdfa136d79f29ab596495e733bb034
Mohammed <m.lahlal@student.avans.nl>
2024-10-17 12:48:22 +0200

    Login Form made

--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/Form1.Designer.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/Form1.Designer.cs
@@ -29,12 +29,117 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.progressBarLoading = new System.Windows.Forms.ProgressBar();
+            this.DateLabel = new System.Windows.Forms.Label();
+            this.CloseButton = new System.Windows.Forms.Button();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timeUpdater = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(284, 246);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(135, 20);
+            this.PasswordTextBox.TabIndex = 0;
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(284, 185);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(135, 20);
+            this.UsernameTextBox.TabIndex = 1;
+            // 
+            // progressBarLoading
+            // 
+            this.progressBarLoading.Location = new System.Drawing.Point(141, 2);
+            this.progressBarLoading.Name = "progressBarLoading";
+            this.progressBarLoading.Size = new System.Drawing.Size(461, 23);
+            this.progressBarLoading.TabIndex = 2;
+            // 
+            // DateLabel
+            // 
+            this.DateLabel.AutoSize = true;
+            this.DateLabel.Location = new System.Drawing.Point(163, 121);
+            this.DateLabel.Name = "DateLabel";
+            this.DateLabel.Size = new System.Drawing.Size(0, 13);
+            this.DateLabel.TabIndex = 3;
+            // 
+            // CloseButton
+            // 
+            this.CloseButton.Location = new System.Drawing.Point(607, 67);
+            this.CloseButton.Name = "CloseButton";
+            this.CloseButton.Size = new System.Drawing.Size(75, 23);
+            this.CloseButton.TabIndex = 4;
+            this.CloseButton.Text = "Close";
+            this.CloseButton.UseVisualStyleBackColor = true;
+            this.CloseButton.Click += new System.EventHandler(this.CloseButton_Click);
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(312, 301);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "Inloggen";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(152, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Uw Gebruikersnaam: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 252);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Uw Wachtwoord: ";
+            // 
+            // timeUpdater
+            // 
+            this.timeUpdater.Interval = 1000;
+            this.timeUpdater.Tick += new System.EventHandler(this.UpdateDateTimeLabel);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(703, 479);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.LoginButton);
+            this.Controls.Add(this.CloseButton);
+            this.Controls.Add(this.DateLabel);
+            this.Controls.Add(this.progressBarLoading);
+            this.Controls.Add(this.UsernameTextBox);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Name = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.ProgressBar progressBarLoading;
+        private System.Windows.Forms.Label DateLabel;
+        private System.Windows.Forms.Button CloseButton;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timeUpdater;
     }
 }
-
--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/Form1.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/Form1.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +9,87 @@ namespace DoctorApplication
 {
     public partial class Form1 : Form
     {
+        private SslStream sslStream;
+        private TcpClient doctorClient;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+
+            
+            if (username == "doctor" && password == "doctor123")
+            {
+                try
+                {
+                    
+                    doctorClient = new TcpClient("localhost", 4790);
+
+                    
+                    sslStream = new SslStream(doctorClient.GetStream(), false, new RemoteCertificateValidationCallback(ValidateServerCertificate), null);
+
+                    
+                    sslStream.AuthenticateAsClient("localhost");
+
+                    
+                    MessageBox.Show("Beveiligde verbinding gemaakt met de server!");
+
+                    
+                    ClientenForm clientenForm = new ClientenForm();
+                    this.Hide();
+                    clientenForm.Show();
+                }
+                catch (Exception ex)
+                {
+                   
+                    MessageBox.Show("Kan geen verbinding maken: " + ex.Message);
+                }
+            }
+            else
+            {
+                
+                MessageBox.Show("Ongeldige gebruikersnaam of wachtwoord.");
+            }
+        }
+
+        // Certificaat validatiefunctie
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+
+        private async void Form1_Load(object sender, EventArgs e)
+        {
+            
+            progressBarLoading.Style = ProgressBarStyle.Marquee;
+            progressBarLoading.MarqueeAnimationSpeed = 30;
+
+            await Task.Delay(3000);
+
+            progressBarLoading.Visible = false;
+
+            
+            timeUpdater.Interval = 1000;  
+            timeUpdater.Tick += new EventHandler(UpdateDateTimeLabel);  
+            timeUpdater.Start();  
+        }
+
+        private void UpdateDateTimeLabel(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            DateLabel.Text = $"Vandaag is het: {now.ToString("dddd, dd MMMM yyyy")}, Tijd: {now.ToString("HH:mm:ss")}";
+        }
+
+  
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();  
         }
     }
 }
