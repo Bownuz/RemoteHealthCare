bf893f6aadd58feb8f81ccb1858e1d6d15893978
Mohammed <m.lahlal@student.avans.nl>
2024-10-17 13:11:54 +0200

    second Form made

--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.Designer.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.Designer.cs
@@ -28,12 +28,75 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.PatientName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Resistance = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.LastUpdateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HeartRate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Speed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.PatientName,
+            this.Resistance,
+            this.LastUpdateColumn,
+            this.HeartRate,
+            this.Speed});
+            this.dataGridView1.Location = new System.Drawing.Point(115, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(552, 138);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // PatientName
+            // 
+            this.PatientName.HeaderText = "Patiëntnaam: ";
+            this.PatientName.Name = "PatientName";
+            // 
+            // Resistance
+            // 
+            this.Resistance.HeaderText = "Weerstand niveau: ";
+            this.Resistance.Name = "Resistance";
+            // 
+            // LastUpdateColumn
+            // 
+            this.LastUpdateColumn.HeaderText = "Laatste Update";
+            this.LastUpdateColumn.Name = "LastUpdateColumn";
+            // 
+            // HeartRate
+            // 
+            this.HeartRate.HeaderText = "Hartslag in bpm : ";
+            this.HeartRate.Name = "HeartRate";
+            // 
+            // Speed
+            // 
+            this.Speed.HeaderText = "Snelheid in km/h : ";
+            this.Speed.Name = "Speed";
+            // 
+            // ClientenForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "ClientenForm";
             this.Text = "ClientenForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PatientName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Resistance;
+        private System.Windows.Forms.DataGridViewTextBoxColumn LastUpdateColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HeartRate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Speed;
     }
 }
