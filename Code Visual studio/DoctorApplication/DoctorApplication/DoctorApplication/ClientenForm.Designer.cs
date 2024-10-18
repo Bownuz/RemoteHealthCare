dc58c07abe16f329bd6eff258c2155d0a42b4025
Mohammed <m.lahlal@student.avans.nl>
2024-10-18 17:12:58 +0200

    Implemented the chat feature from doctor

--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.Designer.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.Designer.cs
@@ -1,7 +1,5 @@
-﻿namespace DoctorApplication
-{
-    partial class ClientenForm
-    {
+﻿namespace DoctorApplication {
+    partial class ClientenForm {
         /// <summary>
         /// Required designer variable.
         /// </summary>
@@ -11,10 +9,8 @@
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing && (components != null))
-            {
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
                 components.Dispose();
             }
             base.Dispose(disposing);
@@ -26,14 +22,16 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
+        private void InitializeComponent() {
             this.ClientenGridView = new System.Windows.Forms.DataGridView();
             this.PatientName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Resistance = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastUpdateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.HeartRate = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Speed = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.SelectClientColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.MessageTextBox = new System.Windows.Forms.TextBox();
+            this.SendMessageButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.ClientenGridView)).BeginInit();
             this.SuspendLayout();
             // 
@@ -45,10 +43,11 @@
             this.Resistance,
             this.LastUpdateColumn,
             this.HeartRate,
-            this.Speed});
-            this.ClientenGridView.Location = new System.Drawing.Point(115, 41);
+            this.Speed,
+            this.SelectClientColumn});
+            this.ClientenGridView.Location = new System.Drawing.Point(83, 45);
             this.ClientenGridView.Name = "ClientenGridView";
-            this.ClientenGridView.Size = new System.Drawing.Size(541, 58);
+            this.ClientenGridView.Size = new System.Drawing.Size(600, 158);
             this.ClientenGridView.TabIndex = 0;
             this.ClientenGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.ClientenGridView_CellContentClick);
             // 
@@ -77,26 +76,56 @@
             this.Speed.HeaderText = "Snelheid in km/h : ";
             this.Speed.Name = "Speed";
             // 
+            // SelectClientColumn
+            // 
+            this.SelectClientColumn.HeaderText = "Selecteer";
+            this.SelectClientColumn.Name = "SelectClientColumn";
+            this.SelectClientColumn.Width = 60;
+            // 
+            // MessageTextBox
+            // 
+            this.MessageTextBox.Location = new System.Drawing.Point(83, 329);
+            this.MessageTextBox.Name = "MessageTextBox";
+            this.MessageTextBox.Size = new System.Drawing.Size(529, 20);
+            this.MessageTextBox.TabIndex = 1;
+            // 
+            // SendMessageButton
+            // 
+            this.SendMessageButton.Location = new System.Drawing.Point(636, 327);
+            this.SendMessageButton.Name = "SendMessageButton";
+            this.SendMessageButton.Size = new System.Drawing.Size(75, 23);
+            this.SendMessageButton.TabIndex = 2;
+            this.SendMessageButton.Text = "Verzenden";
+            this.SendMessageButton.UseVisualStyleBackColor = true;
+            this.SendMessageButton.Click += new System.EventHandler(this.SendMessageButton_Click);
+            // 
             // ClientenForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SendMessageButton);
+            this.Controls.Add(this.MessageTextBox);
             this.Controls.Add(this.ClientenGridView);
             this.Name = "ClientenForm";
             this.Text = "ClientenForm";
+            this.Load += new System.EventHandler(this.ClientenForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ClientenGridView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView ClientenGridView;  
+        private System.Windows.Forms.DataGridView ClientenGridView;
         private System.Windows.Forms.DataGridViewTextBoxColumn PatientName;
         private System.Windows.Forms.DataGridViewTextBoxColumn Resistance;
         private System.Windows.Forms.DataGridViewTextBoxColumn LastUpdateColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn HeartRate;
         private System.Windows.Forms.DataGridViewTextBoxColumn Speed;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn SelectClientColumn;
+        private System.Windows.Forms.TextBox MessageTextBox;
+        private System.Windows.Forms.Button SendMessageButton;
     }
 }
--- Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.cs
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/ClientenForm.cs
@@ -1,55 +1,72 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace DoctorApplication
-{
-    public partial class ClientenForm : Form
-    {
-        public ClientenForm()
-        {
+namespace DoctorApplication {
+    public partial class ClientenForm : Form {
+        private ServerConnection serverConnection;
+
+        public ClientenForm() {
             InitializeComponent();
             serverConnection = new ServerConnection();
             serverConnection.OnDataReceived += UpdateDataGridView;
-            //this.Load += new System.EventHandler(this.ClientenForm_Load);
-
         }
-        private async void ClientenForm_Load(object sender, EventArgs e)
-        {
-            try
-            {
-                // Verbind met de server
+
+        private async void ClientenForm_Load(object sender, EventArgs e) {
+            try {
                 await serverConnection.ConnectToServer("localhost", 4790);
             }
-            catch (Exception ex)
-            {
+            catch(Exception ex) {
                 MessageBox.Show(ex.Message);
             }
         }
 
-        //private void ClientenForm_Load(object sender, EventArgs e)
-        //{
-        //    ClientenGridView.Rows.Add("John Doe", "5", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), "72", "22");
-        //    ClientenGridView.Rows.Add("Jane Smith", "3", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), "85", "25");
-        //    ClientenGridView.Rows.Add("Sam Wilson", "4", DateTime.Now.ToString("dd-MM-yyyy HH:mm:ss"), "90", "18");
-        //}
-        private void ClientenGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {       
+        private void SendMessageButton_Click(object sender, EventArgs e) {
+            string message = MessageTextBox.Text;
+            string selectedClient = null;
+
+            // Zoek de geselecteerde client
+            foreach(DataGridViewRow row in ClientenGridView.Rows) {
+                bool isSelected = Convert.ToBoolean(row.Cells["SelectClientColumn"].Value);
+                if(isSelected) {
+                    selectedClient = row.Cells["PatientName"].Value.ToString();
+                    break;
+                }
+            }
+
+            if(selectedClient != null) {
+                try {
+                    // Stuur het bericht naar de server, die het vervolgens naar de cliënt stuurt
+                    serverConnection.SendMessageToServer(message, selectedClient);
+                    MessageBox.Show($"Bericht verzonden naar {selectedClient} via de server.");
+                }
+                catch(Exception ex) {
+                    MessageBox.Show("Fout bij het verzenden van het bericht: " + ex.Message);
+                }
+            }
+            else {
+                MessageBox.Show("Selecteer een cliënt om een bericht naar te sturen.");
+            }
         }
-        private void UpdateDataGridView(ClientData clientData)
-        {
-    
-            foreach (DataGridViewRow row in ClientenGridView.Rows)
-            {
-                if (row.Cells["PatientName"].Value?.ToString() == clientData.PatientName)
-                {
-                    // Update bestaande rij
+
+        private void ClientenGridView_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+            // Controleer of de cel een checkbox is en update de selectie
+            if(e.ColumnIndex == ClientenGridView.Columns["SelectClientColumn"].Index && e.RowIndex >= 0) {
+                DataGridViewCheckBoxCell checkBoxCell = (DataGridViewCheckBoxCell)ClientenGridView.Rows[e.RowIndex].Cells["SelectClientColumn"];
+                checkBoxCell.Value = !(checkBoxCell.Value != null && (bool)checkBoxCell.Value);
+            }
+        }
+
+        private void UpdateDataGridView(ClientData clientData) {
+            foreach(DataGridViewRow row in ClientenGridView.Rows) {
+                if(row.Cells["PatientName"].Value?.ToString() == clientData.PatientName) {
                     row.Cells["Resistance"].Value = clientData.Resistance;
                     row.Cells["HeartRate"].Value = clientData.HeartRate;
                     row.Cells["Speed"].Value = clientData.Speed;
                     row.Cells["LastUpdateColumn"].Value = clientData.LastUpdate;
                     return;
                 }
-            }         
+            }
+
             ClientenGridView.Rows.Add(
                 clientData.PatientName,
                 clientData.Resistance,
@@ -60,5 +77,3 @@ namespace DoctorApplication
         }
     }
 }
-
-    
