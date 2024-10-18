f1a36881c1bc648e767b95224fb058264c4781e5
Mohammed <m.lahlal@student.avans.nl>
2024-10-18 18:45:07 +0200

    third Form made to illustrate the data in a chart

---
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/TrainingDataForm.Designer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DoctorApplication {
+    partial class TrainingDataForm {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent() {
+            this.BackToClientenFormButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // BackToClientenFormButton
+            // 
+            this.BackToClientenFormButton.Location = new System.Drawing.Point(12, 12);
+            this.BackToClientenFormButton.Name = "BackToClientenFormButton";
+            this.BackToClientenFormButton.Size = new System.Drawing.Size(120, 23);
+            this.BackToClientenFormButton.TabIndex = 0;
+            this.BackToClientenFormButton.Text = "Terug naar Cliënten";
+            this.BackToClientenFormButton.UseVisualStyleBackColor = true;
+            this.BackToClientenFormButton.Click += new System.EventHandler(this.BackToClientenFormButton_Click);
+            // 
+            // TrainingDataForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BackToClientenFormButton);
+            this.Name = "TrainingDataForm";
+            this.Text = "TrainingDataForm";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BackToClientenFormButton;
+    }
+}
---
+++ Code Visual studio/DoctorApplication/DoctorApplication/DoctorApplication/TrainingDataForm.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Security;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace DoctorApplication {
+    public partial class TrainingDataForm : Form {
+        private List<TrainingData> trainingData;
+        private Chart trainingChart;
+        private SslStream sslStream;
+
+        public TrainingDataForm(List<TrainingData> data, SslStream sslStream) {
+            InitializeComponent();
+            trainingData = data;
+            this.sslStream = sslStream;
+            InitializeChart();
+        }
+
+        private void BackToClientenFormButton_Click(object sender, EventArgs e) {
+            ClientenForm clientenForm = new ClientenForm(sslStream);
+            this.Hide();
+            clientenForm.Show();
+        }
+
+        private void InitializeChart() {
+            trainingChart = new Chart {
+                Dock = DockStyle.Fill
+            };
+            ChartArea chartArea = new ChartArea("TrainingDataChartArea");
+            trainingChart.ChartAreas.Add(chartArea);
+            Series series = new Series("TrainingDataSeries") {
+                ChartType = SeriesChartType.Line
+            };
+            trainingChart.Series.Add(series);
+            foreach(var dataPoint in trainingData) {
+                series.Points.AddXY(dataPoint.TimeStamp, dataPoint.Value);
+            }
+            Controls.Add(trainingChart);
+        }
+    }
+
+    public class TrainingData {
+        public DateTime TimeStamp { get; set; }
+        public double Value { get; set; }
+    }
+}
