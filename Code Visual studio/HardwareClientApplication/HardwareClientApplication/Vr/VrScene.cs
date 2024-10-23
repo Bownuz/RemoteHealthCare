fc044bcfe656ae82190120b2581ecd9b4145f6a5
tiemenbr <tiemenbr@hotmail.nl>
2024-10-23 10:59:41 +0200

    added json files
    when sending json objects to server the last property should not have a comma
    for example: {"name":"John", "age":30, "car":null} and not {"name":"John", "age":30, "car":null,}

--- Code Visual studio/HardwareClientApplication/HardwareClientApplication/Vr/VrScene.cs
+++ Code Visual studio/HardwareClientApplication/HardwareClientApplication/Vr/VrScene.cs
@@ -8,11 +8,24 @@ namespace HardwareClientApplication.Vr {
     internal class VrScene {
         // create scene
         public static void NewScene(string ipAdressVrServer) {
-            //VrConnection.NewConnection(ipAdressVrServer);
-            //VrConnection.CreateTunnel();
             VrConnection.NewConnection(ipAdressVrServer, true);
+            VrConnection.SendJsonObjectViaTunnelFromFile("scene_reset.json");
+        }
+
+        public static void GetScene() {
             VrConnection.SendJsonObjectViaTunnelFromFile("scene_get.json");
         }
+
+        public static void NewNodeSceneAndTerrain() {
+            VrConnection.SendJsonObjectViaTunnelFromFile("scene_terrain_add.json");
+            VrConnection.SendJsonObjectViaTunnelFromFile("scene_node_add.json");
+        }
+
+        public static void NewRoute() {
+            VrConnection.SendJsonObjectViaTunnelFromFile("route_add.json");
+        }
+
+        // create route?
         // send data scene
     }
 }
