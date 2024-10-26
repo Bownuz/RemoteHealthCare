6f61d977c4170bd7b8e076c4e605c72900ef413b
davidVeer <davidvdveer05@gmail.com>
2024-10-26 14:46:37 +0200

    Patient states code style clean-up

--- Code Visual studio/ServerTest2.1/ServerTest2.1/Patterns/State/PatientProtocolStates.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/Patterns/State/PatientProtocolStates.cs
@@ -3,84 +3,60 @@ using Server.ThreadHandlers;
 using System.Text.Json;
 
 
-namespace Server.Patterns.State.PatientStates
-{
-    public class P_Welcome : PatientState
-    {
-        public P_Welcome(DataProtocol protocol, PatientHandler patientHandler) : base(protocol, patientHandler)
-        {
-        }
-
-        public override string CheckInput(string input)
-        {
+namespace Server.Patterns.State.PatientStates {
+    public class P_Welcome(DataProtocol protocol, PatientHandler patientHandler) : PatientState(protocol, patientHandler) {
+        public override string CheckInput(string input) {
             protocol.ChangeState(new P_Initialise(protocol, patientHandler));
             return "Welcome Client";
         }
-    }   
+    }
 
-    public class P_Initialise : PatientState
-    {
-        public P_Initialise(DataProtocol protocol, PatientHandler patientHandler) : base(protocol, patientHandler)
-        {
-        }
-
-        public override string CheckInput(string input)
-        {
-            if (input.Equals("Quit Communication"))
-            {
+    public class P_Initialise(DataProtocol protocol, PatientHandler patientHandler) : PatientState(protocol, patientHandler) {
+        public override string CheckInput(string input) {
+            if (input.Equals("Quit Communication")) {
                 return "Goodbye";
             }
 
 
-            if (jsonRegex.IsMatch(input)){
-            PatientInitialisationMessage dataFromMessage = JsonSerializer.Deserialize<PatientInitialisationMessage>(input);
-            Patient connectedPerson;
-            FileStorage storageFromThread = patientHandler.fileStorage;
+            if (jsonRegex.IsMatch(input)) {
+                PatientInitialisationMessage dataFromMessage = JsonSerializer.Deserialize<PatientInitialisationMessage>(input);
+                Patient connectedPerson;
+                FileStorage storageFromThread = patientHandler.fileStorage;
 
-            if (!storageFromThread.PatientExists(dataFromMessage.ClientName))
-            {
-                storageFromThread.AddPatient(dataFromMessage.ClientName);
-            }
-            patientHandler.connectedPatient = storageFromThread.GetPatient(dataFromMessage.ClientName);
-            connectedPerson = patientHandler.connectedPatient;
+                if (!storageFromThread.PatientExists(dataFromMessage.ClientName)) {
+                    storageFromThread.AddPatient(dataFromMessage.ClientName);
+                }
+                patientHandler.connectedPatient = storageFromThread.GetPatient(dataFromMessage.ClientName);
+                connectedPerson = patientHandler.connectedPatient;
 
-            connectedPerson.currentSession =
-                new Session(
-                    dataFromMessage.dateTime,
-                    dataFromMessage.ConnectedErgometer,
-                    dataFromMessage.ConnectedHeartRateMonitor
-                    );
-            connectedPerson.addSession(connectedPerson.currentSession);
-            connectedPerson.currentSession.AddObserver(patientHandler);
-            if (!connectedPerson.currentSession.observers.Contains(storageFromThread))
-            {
-                connectedPerson.currentSession.AddObserver(storageFromThread);
-            }
+                connectedPerson.currentSession =
+                    new Session(
+                        dataFromMessage.dateTime,
+                        dataFromMessage.ConnectedErgometer,
+                        dataFromMessage.ConnectedHeartRateMonitor
+                        );
+                connectedPerson.addSession(connectedPerson.currentSession);
+                connectedPerson.currentSession.AddObserver(patientHandler);
+                if (!connectedPerson.currentSession.observers.Contains(storageFromThread)) {
+                    connectedPerson.currentSession.AddObserver(storageFromThread);
+                }
 
 
-            protocol.ChangeState(new P_RecievingData(protocol, patientHandler));
+                protocol.ChangeState(new P_RecievingData(protocol, patientHandler));
                 return "Ready to recieve data";
             }
             return "add failed message";
         }
-    
+
     }
 
-    public class P_RecievingData : PatientState
-    {
-        public P_RecievingData(DataProtocol protocol, PatientHandler patientHandler) : base(protocol, patientHandler)
-        {
-        }
-
-        public override string CheckInput(string input)
-        {
-            if (input.Equals("Quit Communication"))
-            {
+    public class P_RecievingData(DataProtocol protocol, PatientHandler patientHandler) : PatientState(protocol, patientHandler) {
+        public override string CheckInput(string input) {
+            if (input.Equals("Quit Communication")) {
                 return "Goodbye";
             }
 
-            if (jsonRegex.IsMatch(input))
-            {
+            if (jsonRegex.IsMatch(input)) {
                 patientHandler.connectedPatient.currentSession.addMessage(input, CommunicationType.PATIENT);
                 return "Ready to recieve data";
             }
