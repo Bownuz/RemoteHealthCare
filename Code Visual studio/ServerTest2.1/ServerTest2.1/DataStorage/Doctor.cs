f7d6df1ab61dca2d1a5bc68e6fbb9fee9585077d
davidVeer <davidvdveer05@gmail.com>
2024-10-26 14:28:12 +0200

    Doctor code style clean-up

--- Code Visual studio/ServerTest2.1/ServerTest2.1/DataStorage/Doctor.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/DataStorage/Doctor.cs
@@ -1,16 +1,11 @@
-using System;
+namespace Server.DataStorage {
+    public class Doctor {
+        public String DoctorID { get; set; }
+        public String DoctorName { get; set; }
 
-namespace Server.DataStorage
-{
-	public class Doctor
-	{
-		public String DoctorID { get; set; }
-		public String DoctorName {  get; set; }
+        public String DoctorPassword { get; set; }
 
-		public String DoctorPassword {  get; set; }
-
-        public Doctor(String DoctorID, string DoctorName, string DoctorPassword)
-        {
+        public Doctor(String DoctorID, string DoctorName, string DoctorPassword) {
             this.DoctorID = DoctorID;
             this.DoctorName = DoctorName;
             this.DoctorPassword = DoctorPassword;
