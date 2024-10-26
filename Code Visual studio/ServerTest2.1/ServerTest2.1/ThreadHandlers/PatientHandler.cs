b8d29465669588237877e5b49e5348c96256461a
davidVeer <davidvdveer05@gmail.com>
2024-10-26 14:47:58 +0200

    Patient Handler code style clean-up

--- Code Visual studio/ServerTest2.1/ServerTest2.1/ThreadHandlers/PatientHandler.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/ThreadHandlers/PatientHandler.cs
@@ -1,22 +1,17 @@
 using Server.DataStorage;
-using System;
+using System.Net.Security;
 using System.Net.Sockets;
 
-namespace Server.ThreadHandlers
-{
-    public class PatientHandler : CommunicationHandler
-	{
-		public Patient connectedPatient {  get; set; }
+namespace Server.ThreadHandlers {
+    public class PatientHandler : CommunicationHandler {
+        public Patient connectedPatient { get; set; }
 
         public PatientHandler(FileStorage fileStorage, SslStream sslStream) : base(fileStorage, sslStream) {
-        {
-		    communicationType = CommunicationType.PATIENT;
-		}
+            communicationType = CommunicationType.PATIENT;
+        }
 
-        public override void Update(CommunicationType communicationOrigin, Session session)
-        {
-            if (communicationOrigin == CommunicationType.DOCTOR)
-            {
+        public override void Update(CommunicationType communicationOrigin, Session session) {
+            if (communicationOrigin == CommunicationType.DOCTOR) {
                 Console.WriteLine(session.getLatestMessage(communicationOrigin));
                 MessageCommunication.SendMessage(sslStream, session.getLatestMessage(communicationOrigin));
             }
