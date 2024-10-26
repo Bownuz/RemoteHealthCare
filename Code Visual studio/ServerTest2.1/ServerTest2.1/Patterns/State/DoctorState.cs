202063dd4b777a909eaa3fe0ef2be7ffe17ff161
davidVeer <davidvdveer05@gmail.com>
2024-10-26 14:46:27 +0200

    abstract DoctorState code style clean-up

--- Code Visual studio/ServerTest2.1/ServerTest2.1/Patterns/State/DoctorState.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/Patterns/State/DoctorState.cs
@@ -1,13 +1,10 @@
 using Server.ThreadHandlers;
 
-namespace Server.Patterns.State
-{
-	public abstract class DoctorState : State
-	{
-		protected DoctorHandler doctorHandler;
+namespace Server.Patterns.State {
+    public abstract class DoctorState : State {
+        protected DoctorHandler doctorHandler;
 
-        protected DoctorState(DataProtocol protocol, DoctorHandler doctorHandler) : base(protocol)
-        {
+        protected DoctorState(DataProtocol protocol, DoctorHandler doctorHandler) : base(protocol) {
             this.doctorHandler = doctorHandler;
         }
     }
