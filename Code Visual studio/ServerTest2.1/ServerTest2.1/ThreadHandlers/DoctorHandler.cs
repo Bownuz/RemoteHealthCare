d1feef968d4d5f6f3d9100c329c5548723ed18cd
Mohammed <m.lahlal@student.avans.nl>
2024-10-30 23:37:18 +0100

    Added stoptraining and starttraining, added DoctorDataMessage

--- Code Visual studio/ServerTest2.1/ServerTest2.1/ThreadHandlers/DoctorHandler.cs
+++ Code Visual studio/ServerTest2.1/ServerTest2.1/ThreadHandlers/DoctorHandler.cs
@@ -1,6 +1,6 @@
 using Server.DataStorage;
-using System.Net.Security;
-using System.Net.Sockets;
+using Server.ThreadHandlers;
+using System;
 
 namespace Server.ThreadHandlers {
     public class DoctorHandler : CommunicationHandler {
@@ -11,12 +11,45 @@ namespace Server.ThreadHandlers {
         }
 
         public override void Update(CommunicationType communicationOrigin, Session session) {
-            if (communicationOrigin == CommunicationType.PATIENT) {
+            if(communicationOrigin == CommunicationType.PATIENT) {
                 Console.WriteLine(session.getLatestMessage(communicationOrigin));
                 MessageCommunication.SendMessage(networkStream, session.getLatestMessage(communicationOrigin));
             }
         }
+
+        public void ProcessCommand(string commandJson) {
+            var command = JsonSerializer.Deserialize<DoctorDataMessage>(commandJson);
+
+            switch(command.Action) {
+                case "StartTraining":
+                    StartTraining(command.PatientName);
+                    break;
+                case "StopTraining":
+                    StopTraining(command.PatientName);
+                    break;
+                case "EmergencyStop":
+                    EmergencyStop(command.PatientName, command.newResistance);
+                    break;
+                default:
+                    MessageCommunication.SendMessage(networkStream, "Onbekend commando ontvangen.");
+                    break;
+            }
+        }
+
+        private void StartTraining(string patientName) {
+            var session = fileStorage.GetPatient(patientName)?.currentSession;
+            session?.addMessage($"Training started for {patientName}", CommunicationType.DOCTOR);
+        }
+
+        private void StopTraining(string patientName) {
+            var session = fileStorage.GetPatient(patientName)?.currentSession;
+            session?.addMessage($"Training stopped for {patientName}", CommunicationType.DOCTOR);
+        }
+
+        private void EmergencyStop(string patientName, int resistance) {
+            var session = fileStorage.GetPatient(patientName)?.currentSession;
+            session?.addMessage($"{patientName} ; message: NOODSTOP ; newResistance: {resistance}", CommunicationType.DOCTOR);
+            MessageCommunication.SendMessage(networkStream, $"{patientName} ; message: NOODSTOP ; newResistance: {resistance}");
+        }
     }
 }
-
-
