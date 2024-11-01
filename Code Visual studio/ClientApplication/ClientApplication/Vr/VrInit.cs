dd5e01477de20e4f217e68e0cbe7984e615efd7a
tiemenbr <tiemenbr@hotmail.nl>
2024-11-01 15:45:21 +0100

    added standard init

---
+++ Code Visual studio/ClientApplication/ClientApplication/Vr/VrInit.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientApplication.Vr {
+    internal class VrInit {
+        public static void StandardInit() {
+            VrScene.NewScene("127.0.0.1");
+            VrScene.NewNodeSceneAndTerrain();
+            VrScene.DeleteGroundPlaneNode();
+            VrScene.AddSceneNodeLayer();
+
+            VrScene.NewRoute();
+            VrScene.AddRoadToNewestRoute();
+            VrScene.GetScene();
+
+            VrScene.FollowNewestRoute();
+        }
+    }
+}
