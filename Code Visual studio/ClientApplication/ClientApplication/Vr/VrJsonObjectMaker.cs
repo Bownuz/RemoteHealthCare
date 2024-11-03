37e4c79901577efde23d8d2f002487cdd0c41b44
tiemenbr <tiemenbr@hotmail.nl>
2024-11-03 16:21:46 +0100

    created a hill in the map and added it to the route

--- Code Visual studio/ClientApplication/ClientApplication/Vr/VrJsonObjectMaker.cs
+++ Code Visual studio/ClientApplication/ClientApplication/Vr/VrJsonObjectMaker.cs
@@ -12,9 +12,9 @@ namespace ClientApplication.Vr {
 
         public struct Data {
             public List<int> size { get; }
-            public List<int> heights { get; }
+            public List<double> heights { get; }
 
-            public Data(List<int> size, List<int> heights) {
+            public Data(List<int> size, List<double> heights) {
                 this.size = size; this.heights = heights;
             }
         }
@@ -29,9 +29,35 @@ namespace ClientApplication.Vr {
         }
 
         public static string SceneTerrainAdd(int width, int height) {
-            List<int> heights = new List<int>();
-            for (int i = 0; i < width*height; i++) {
+            List<double> heights = new List<double>();
+            //Random random = new Random();
+            for (int i = 0; i < width * height; i++) {
                 heights.Add(0);
+            }
+            for (int i = 171; i < 210; i++) {
+                double step = 5 - (5 * (209 - i) / 38.0);
+                for (int j = 0; j < 256; j++) {
+                    heights[i*256+j] = step;
+                    //Console.WriteLine(i*256+j);
+                    //Console.WriteLine(5 - (5 * (209 - i) / 38.0));
+                }
+            }
+            for (int i = 210; i < 216; i++) {
+                for (int j = 0; j < 256; j++) {
+                    heights[i * 256 + j] = 5;
+                }
+            }
+            for (int i = 216; i < 250; i++) {
+                double step = 11 - (5 * (256 - i) / 34.0);
+                Console.WriteLine(10 - (5 * (256 - i) / 34.0));
+                for (int j = 0; j < 256; j++) {
+                    heights[i * 256 + j] = step;
+                }
+            }
+            for (int i = 250; i < 256; i++) {
+                for (int j = 0; j < 256; j++) {
+                    heights[i * 256 + j] = 10;
+                }
             }
             Data data = new Data(new List<int> { width, height },heights);
             return JsonSerializer.Serialize(new ST("scene/terrain/add",data));
